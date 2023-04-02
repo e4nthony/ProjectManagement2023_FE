68de8e97423fee024c682cebdad4579af86fa06f
StavShar <stavshar21@gmail.com>
2023-04-02 16:11:34 +0300

    disable linter in HomePage.jsx (temporary)

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable */
+/* the line above disables eslint check for this file (temporarily) todo:delete*/
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import LoginUi from "./LoginUI";
