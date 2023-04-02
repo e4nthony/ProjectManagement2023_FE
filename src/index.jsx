818f9ded3dd8fd432a75bc9aa50eacce2eb8c878
Anthony E <e4nthony@gmail.com>
2023-04-02 21:12:29 +0300

    enable the index.css styles file

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';    //not in use
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 import App from './App';
