58efcc230388e3c5cd34d962e0106d66e0e46441
StavShar <stavshar21@gmail.com>
2023-04-03 19:37:15 +0300

    formatting test file

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -4,14 +4,7 @@ describe("Array", () => {
   describe("#sort", () => {
     it("should sorting array by name", () => {
       var names = ["Anthony", "Stav", "Adar", "Ido", "Maor", "Neoray"];
-      expect(names.sort()).to.be.eql([
-        "Adar",
-        "Anthony",
-        "Ido",
-        "Maor",
-        "Neoray",
-        "Stav",
-      ]);
+      expect(names.sort()).to.be.eql(["Adar", "Anthony", "Ido", "Maor", "Neoray", "Stav"]);
     });
   });
 });
