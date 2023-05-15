633a3c26e77269d2c0255e0e7bd63bc208d2f6d1
Maor Hadad <maorhadad94@gmail.com>
2023-05-15 19:24:13 +0300

    Changing the site name in the navbar and adding a logo image

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav className='nav'>
             <a href='/' className='site-title'>
-                SCRUM
+                BidZone
             </a>
             <ul>
                 <li className='active'>
