9bc9b17caca965350d0fe3fdab62e7c122b8f841
hazanido <hazanido@gmail.com>
2023-05-20 17:10:15 +0300

    register (fix) work

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable */
+/* the line above disables eslint check for this file (temporarily) todo:delete */
+
 import React, { useState } from 'react';
 import './styles/style.css';
 import { useNavigate } from 'react-router-dom';
@@ -7,195 +10,204 @@ import register_model from '../../model/register_model';
 
 
 function RegistrationPage() {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [dateOfBirth, setDateOfBirth] = useState('');
-    const [passwordMatchError, setPasswordMatchError] = useState('');
-    const [ageError, setAgeError] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const [matchMessage, setMatchMessage] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState('');
+  const [passwordMatchError, setPasswordMatchError] = useState('');
+  const [ageError, setAgeError] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [matchMessage, setMatchMessage] = useState('');
 
-    //event.target.value
-    const handleConfirmPasswordChange = (event) => {
-      setConfirmPassword(event.target.value);
-      if (password !== event.target.value) {
-        setPasswordMatchError('The password not match');
-        setMatchMessage('');
-      } else {
-        setMatchMessage('The password match');
-        setPasswordMatchError('');
-        }
-        if (password.length < 6) {
-          setErrorMessage('Password is too short, please enter password from 6 to 18 characters');
-        } else if (password.length > 18) {
-        setErrorMessage('Password is too long, please enter password from 6 to 18 characters');
-        } else {
-          setErrorMessage('')
-        }
-    };
-    const handelEmail = (event) => {
-   
-      setEmail(event.target.value);
-       if (event.target.value.includes('@') && event.target.value.includes('.')) {
+  //event.target.value
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+    if (password !== event.target.value) {
+      setPasswordMatchError('The password not match');
+      setMatchMessage('');
+    } else {
+      setMatchMessage('The password match');
+      setPasswordMatchError('');
+    }
+    if (password.length < 6) {
+      setErrorMessage('Password is too short, please enter password from 6 to 18 characters');
+    } else if (password.length > 18) {
+      setErrorMessage('Password is too long, please enter password from 6 to 18 characters');
+    } else {
+      setErrorMessage('')
+    }
+  };
+  const handelEmail = (event) => {
+
+    setEmail(event.target.value);
+    if (event.target.value.includes('@') && event.target.value.includes('.')) {
       setErrorMessage('');
-        } else {
+    } else {
       setErrorMessage('Please input a valid Email');
     }
 
-    };
-    const handelDateOfBirth = (event) => {
-      setDateOfBirth(event.target.value) 
-      // Check if user is over 18 years old
-       const dobDate = new Date(event.target.value);
-       const nowDate = new Date();
-       const diffInYears = (nowDate - dobDate) / (1000 * 60 * 60 * 24 * 365);
-       if (diffInYears < 18) {
-         setAgeError('You must be at least 18 years old to register');
-       } else {
-        setAgeError('');
-      }
+  };
+  const handelDateOfBirth = (event) => {
+    setDateOfBirth(event.target.value)
+    // Check if user is over 18 years old
+    const dobDate = new Date(event.target.value);
+    const nowDate = new Date();
+    const diffInYears = (nowDate - dobDate) / (1000 * 60 * 60 * 24 * 365);
+    if (diffInYears < 18) {
+      setAgeError('You must be at least 18 years old to register');
+    } else {
+      setAgeError('');
+    }
 
-    };
+  };
 
-    const handelPassowrd = (event) => {
-      setPassword(event.target.value);
-      if (event.target.value !== confirmPassword) {
-        setPasswordMatchError('The password not match');
-        setMatchMessage('');
-      } else {
-        setMatchMessage('The password match');
-        setPasswordMatchError('');
-        }
+  const handelPassowrd = (event) => {
+    setPassword(event.target.value);
+    if (event.target.value !== confirmPassword) {
+      setPasswordMatchError('The password not match');
+      setMatchMessage('');
+    } else {
+      setMatchMessage('The password match');
+      setPasswordMatchError('');
+    }
 
-    };
-    
-
-
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-
-        if (email === '' || password === '' || username === '' || firstName === '' || lastName === '' || dateOfBirth === '') {
-          setErrorMessage('Please fill all fields');
-        } else {
-          setErrorMessage('');
-        }
+  };
 
 
-        // Create form data object
-        const formData = new FormData();
-        formData.append('firstName', firstName);
-        formData.append('lastName', lastName);
-        formData.append('username', username);
-        formData.append('email', email);
-        formData.append('password', password);
-        formData.append('dateOfBirth', dateOfBirth);
 
-        // Send form data to server
-    
-        let is_success;
-        try {
-            is_success = await register_model.register(formData);
-        } catch (err) {
-            console.log('failed to log in user: ' + err);
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        if (!is_success) {
-            errorMessage();
-            return;
-        }
-        useNavigate('/home');
+    if (email === '' || password === '' || username === '' || firstName === '' || lastName === '' || dateOfBirth === '') {
+      setErrorMessage('Please fill all fields');
+    } else {
+      setErrorMessage('');
+    }
 
-    };
+    // Create form data object
+    const formData = new FormData();
+    formData.append('firstName', firstName);
+    formData.append('lastName', lastName);
+    formData.append('userName', username);
+    formData.append('email', email);
+    //formData.append('password', password);
+    formData.append('date', dateOfBirth);
 
-    
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor='firstName'>
+    const data = {
+      firstName: firstName,
+      lastName: lastName,
+      userName: username,
+      email: email,
+      date: dateOfBirth
+    }
+
+   
+
+    // Send form data to server
+
+    let is_success;
+    try {
+      is_success = await register_model.register(data);
+    } catch (err) {
+      console.log('failed to log in user: ' + err);
+    }
+
+    if (!is_success) {
+      errorMessage();
+      return;
+    }
+    useNavigate('/home');
+
+  };
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor='firstName'>
         First Name:
-                <input
-                  type='text'
-                  id='firstName'
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                />
-            </label>
-            <br />
-            <label htmlFor='lastName'>
+        <input
+          type='text'
+          id='firstName'
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </label>
+      <br />
+      <label htmlFor='lastName'>
         Last Name:
-                <input
-                  type='text'
-                  id='lastName'
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
-                />
-            </label>
-            <br />
+        <input
+          type='text'
+          id='lastName'
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </label>
+      <br />
 
-            <label htmlFor='email' className='label'>
+      <label htmlFor='email' className='label'>
         Email
-                <input
-                  type='email'
-                  id='email'
-                  className='input'
-                  value={email}
-                  onChange={handelEmail}
-                  required
-                />
-            </label>
+        <input
+          type='email'
+          id='email'
+          className='input'
+          value={email}
+          onChange={handelEmail}
+          required
+        />
+      </label>
 
-            <label htmlFor='username'>
+      <label htmlFor='username'>
         Username:
-                <input
-                  type='text'
-                  id='username'
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-            </label>
-            <br />
-            <label htmlFor='password'>
+        <input
+          type='text'
+          id='username'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <br />
+      <label htmlFor='password'>
         Password:
-                <input
-                  type='password'
-                  id='password'
-                  value={password}
-                  onChange={handelPassowrd}
-                />
-            </label>
-            <br />
-            <label htmlFor='confirmPassword'>
+        <input
+          type='password'
+          id='password'
+          value={password}
+          onChange={handelPassowrd}
+        />
+      </label>
+      <br />
+      <label htmlFor='confirmPassword'>
         Confirm Password:
-                <input
-                  type='password'
-                  id='confirmPassword'
-                  value={confirmPassword}
-                  onChange={handleConfirmPasswordChange}
-                />
-            </label>
-            {passwordMatchError && <p style={{ color: 'red' }}>{passwordMatchError}</p>}
-            {matchMessage && <p style={{ color: 'green' }}>{matchMessage}</p>}
-            <br />
-            <label htmlFor='dateOfBirth'>
+        <input
+          type='password'
+          id='confirmPassword'
+          value={confirmPassword}
+          onChange={handleConfirmPasswordChange}
+        />
+      </label>
+      {passwordMatchError && <p style={{ color: 'red' }}>{passwordMatchError}</p>}
+      {matchMessage && <p style={{ color: 'green' }}>{matchMessage}</p>}
+      <br />
+      <label htmlFor='dateOfBirth'>
         Date of Birth:
-                <input
-                  type='date'
-                  id='dateOfBirth'
-                  value={dateOfBirth}
-                  onChange={handelDateOfBirth}
-                />
-            </label>
-            <br />
+        <input
+          type='date'
+          id='dateOfBirth'
+          value={dateOfBirth}
+          onChange={handelDateOfBirth}
+        />
+      </label>
+      <br />
 
-            {ageError && <p style={{ color: 'black' }}>{ageError }</p>}
-            {errorMessage && <p style={{ color: 'black' }}>{errorMessage }</p>}
+      {ageError && <p style={{ color: 'black' }}>{ageError}</p>}
+      {errorMessage && <p style={{ color: 'black' }}>{errorMessage}</p>}
 
-            <button type='submit' onClick={handleSubmit}>Register</button>
-        </form>
-    );
+      <button type='submit' onClick={handleSubmit}>Register</button>
+    </form>
+  );
 }
 
 export default RegistrationPage;
