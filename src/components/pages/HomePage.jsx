8aaf3328112e71585a32affc81055cd5edaecda7
StavShar <stavshar21@gmail.com>
2023-05-20 17:45:54 +0300

    added logo

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { useNavigate } from 'react-router-dom';
 import './styles/pages_styles.css';
+import logo from '../../pictures/BidZone-logo.png';
 
 function HomePage() {
     const navigate = useNavigate();
@@ -13,6 +14,7 @@ function HomePage() {
     return (
         <div className='main-container'>
             <h1>Home page !</h1>
+            <img src={logo} alt='' />
             <p className='about-par'>
                 Welcome to our project in Project Managment course 2023
             </p>
--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -22,7 +22,7 @@ export default function Navbar() {
     return (
         <nav className='nav'>
             <a href='/' className='site-title'>
-                SCRUM
+                BidZone
             </a>
             <ul>
                 {!authState && (
