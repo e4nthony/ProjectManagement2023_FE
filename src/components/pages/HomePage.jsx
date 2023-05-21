5faf0cd34839e49eccdfac2fc3b901b17fe988e0
StavShar <stavshar21@gmail.com>
2023-05-21 13:40:09 +0300

    פosts are displayed on the home page

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -5,10 +5,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { useNavigate } from 'react-router-dom';
-import './styles/pages_styles.css';
+import './styles/HomePage.css';
 import logo from '../../pictures/BidZone-logo.png';
 
 function HomePage() {
+    const post1 = {
+        title: 'post1Title',
+        postText: 'post1Text',
+        postMinPrice: 'post1minPrice',
+        postMaxTime: 'post1maxTime',
+        postImage: 'post1Image',
+        username: 'user1',
+    };
+    const post2 = {
+        title: 'post2Title',
+        postText: 'post2Text',
+        postMinPrice: 'post2minPrice',
+        postMaxTime: 'post2maxTime',
+        postImage: 'post2Image',
+        username: 'user2',
+    };
+    const posts = [post1, post2];
+
     const navigate = useNavigate();
 
     function handleClick() {
@@ -17,11 +35,31 @@ function HomePage() {
 
     return (
         <div className='main-container'>
-            <h1>Home page !</h1>
-            <img src={logo} alt='' />
-            <p className='about-par' style={{ color: 'black' }}>
-                Welcome to our project in Project Managment course 2023
-            </p >
+            <div>
+                <h1>Home page !</h1>
+                <div><img src={logo} alt='' /></div>
+                <p className='about-par' style={{ color: 'black' }}>
+                    Welcome to our project in Project Managment course 2023
+                </p >
+                <div className='posts'>
+                    {posts.map((post) => {
+                        return (
+                            <div
+                                className="post"
+                                onClick={() => {
+                                    console.log(post.title + ' has been clicked')
+                                }} >
+                                <div className="title"> {post.title} </div>
+                                <div className="body">{post.postText}</div>
+                                <div className="body">{post.postImage}</div>
+                                <div className="footer">{post.postMinPrice}</div>
+                                <div className="footer">{post.postMaxTime}</div>
+                                <div className="footer">{post.username}</div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
         </div>
     );
 }
