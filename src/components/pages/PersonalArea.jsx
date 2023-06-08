d62033ba1a5c91d22958595843ec5e91fe4317ad
neo050 <neoray.asking666@gmail.com>
2023-06-08 13:19:00 +0300

    Adding a rating to the user in the personal area

--- src/components/pages/PersonalArea.jsx
+++ src/components/pages/PersonalArea.jsx
@@ -1,107 +1,145 @@
-
 /* eslint-disable */
 /* the line above disables eslint check for this file (temporarily) todo:delete */
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import authService from '../../services/authService';
 import Rating from 'react-rating';
 
-import React, { useState } from 'react';
-import {
-    Link, Route, Routes, useNavigate   
-} from 'react-router-dom';
-// import './styles/personalArea.css';
-import Login from './Login';
-import authService from '../../services/authService';
-import { async } from 'q';
 function PersonalArea() {
-    const [isLoggedIn, setIsLoggedIn] = useState(true); // change to false when log in
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [dateOfBirth, setDateOfBirth] = useState('2000-01-01');
-    const [userName,setUserName] = useState('')
-    async function getinfo()
-    {
-    const res = await authService.get_user_info_by_email(localStorage.getItem('activeUserEmail'))
-    console.log(res);
-        setUserName(res.data.user_info.userName);
-        setDateOfBirth(res.data.user_info.birth_date);
-        setFirstName(res.data.user_info.firstName);
-        setLastName(res.data.user_info.lastName);
+  const [isLoggedIn, setIsLoggedIn] = useState(true); // change to false when log in
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState('2000-01-01');
+  const [userName, setUserName] = useState('');
+  const [rating, setRating] = useState(0); // New rating state
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false); // State to control success message
+  const [averageRating, setAverageRating] = useState(0); // State for average rating
 
-        
+  const handleRatingChange = (value) => {
+    setRating(value);
+    handleRatingSubmit(value);
+  };
+
+  const handleRatingSubmit = async (value) => {
+    try {
+        console.log('bbb:', email);
+
+      // Check if user is trying to rate themselves
+      if (localStorage.getItem('activeUserEmail') !== email) {
+        // TODO: Send the rating to the server and save it in the MongoDB database
+        console.log('Rating:', email);
+
+        // Show the success message
+        setShowSuccessMessage(true);
+
+        // Clear the success message after 10/4 seconds
+        setTimeout(() => {
+          setShowSuccessMessage(false);
+        }, 2500);
+      } else {
+        console.log('You cannot rate yourself.');
+      }
+    } catch (error) {
+      console.error('Error saving rating:', error);
     }
-    getinfo();
-    console.log(userName);
-    const handleRatingChange = (value) => {
-        // התנהגות לאחר שינוי בדירוג
-        console.log('הדירוג החדש הוא: ' + value);
-      };
-    return (
+  };
+
+  async function getInfo() { 
+    let email1 =localStorage.getItem('activeUserEmail')
+    console.log("pppp",email1.toString());
+    const res = await authService.get_user_info_by_email(email1);//to do 
+    console.log(res);
+    setEmail( res.data.user_info.email);
+    setUserName(res.data.user_info.userName);
+    setDateOfBirth(res.data.user_info.birth_date);
+    setFirstName(res.data.user_info.firstName);
+    setLastName(res.data.user_info.lastName);
+    // TODO: Retrieve ratings from MongoDB and calculate the average rating
+    // Replace the following line with the logic to calculate the average rating
+    const ratings = [4, 5, 3, 4]; // Example ratings array
+    const totalRating = ratings.reduce((sum, rating) => sum + rating, 0);
+    const averageRating = totalRating / ratings.length || 0;
+    setAverageRating(averageRating);
+  }
+
+  useEffect(() => {
+    getInfo();
+  }, []);
+
+  return (
+    <div>
+      <h1>Personal Area</h1>
+      {isLoggedIn ? (
         <div>
-            <h1>Personal Area</h1>
-            {isLoggedIn ? (
-                <div>
-                    {/* User Information */}
-                    <div>
-                        <strong>First Name: {firstName} </strong>
-                        
-                    </div>
-                    <div>
-                        <strong>Last Name:  {lastName} </strong>
+          {/* User Information */}
+          <div>
+            <strong>First Name: {firstName} </strong>
+          </div>
+          <div>
+            <strong>Last Name: {lastName} </strong>
+          </div>
+          <div>
+            <strong>Username: </strong>
+            {userName}
+          </div>
+          <div>
+            <strong>Email: </strong>
+            {email}
+          </div>
+          <div>
+            <strong>Password: </strong>
+            ********
+          </div>
+          {/* Following and Followers */}
+          <div className="following-followers">
+            <div className="following-followers-item">
+              <h3>Following</h3>
+              <span>{}</span>
+            </div>
+            <div className="following-followers-item">
+              <h3>Followers</h3>
+              <span>{}</span>
+            </div>
+          </div>
+          {/* Rating */}
+          {localStorage.getItem('activeUserEmail') !== email && ( // Check if user is not rating themselves
+            <div>
+              <label>Rate the user:</label>
+              <Rating
+                initialRating={rating}
+                emptySymbol={<span className="rating-icon">&#9734;</span>}
+                fullSymbol={<span className="rating-icon selected">&#9733;</span>}
+                onChange={handleRatingChange}
+              />
+            </div>
+          )}
+          {/* Average Rating */}
+          <div>
+            <strong>Average Rating: </strong>
+            {averageRating.toFixed(1)} stars
+          </div>
+          {/* Success Message */}
+          {showSuccessMessage && <div className="success-message">Rating submitted successfully!</div>}
+          {/* Edit Info and Delete Account buttons */}
+          <Link to="/personalarea/editinfo">
+            <button type="button">Edit Info</button>
+          </Link>
 
-                    </div>
-                    <div>
-                        <strong>Username: </strong>
-                        {userName}
-                    </div>
-                    <div>
-                        <strong>Email: </strong>
-                        {localStorage.getItem('activeUserEmail')}
-                    </div>
-                    <div>
-                        <strong>Password: </strong>
-                          ********
-                    </div>
-                    <div>
-                    <strong>Rating:</strong>
-                    <br></br>
-                    <Rating
-                        placeholderRating={3.5}
-                        emptySymbol={<img src="assets/images/star-grey.png" className="icon" alt="כוכב ריק" />}
-                        placeholderSymbol={<img src="assets/images/star-red.png" className="icon" alt="כוכב ממולא חלקית" />}
-                        fullSymbol={<img src="assets/images/star-yellow.png" className="icon" alt="כוכב מלא" />}
-                        onChange={handleRatingChange}
-                    />
-                    </div>
-                    {/* Following and Followers */}
-                    <div className='following-followers'>
-                        <div className='following-followers-item'>
-                            <h3>Following</h3>
-                            <span>{}</span>
-                        </div>
-                        <div className='following-followers-item'>
-                            <h3>Followers</h3>
-                            <span>{}</span>
-                        </div>
-                    </div>
-                    <Link to='/personalarea/editinfo'>
-                        <button type='button'>Edit Info</button>
-                    </Link>
-
-                    <Link to='/personalarea/confirmanddelet'>
-                    <button type='button' >delete my account</button>
-                    </Link>
-
-
-                </div>
-            ) : (
-                <div>
-          Please log in to view your information.
-                    <Link to='/login'>
-                        <button type='button'>Login</button>
-                    </Link>
-                </div>
-            )}
+          <Link to="/personalarea/confirmanddelet">
+            <button type="button">Delete My Account</button>
+          </Link>
         </div>
-    );
+      ) : (
+        <div>
+          Please log in to view your information.
+          <Link to="/login">
+            <button type="button">Login</button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default PersonalArea;
