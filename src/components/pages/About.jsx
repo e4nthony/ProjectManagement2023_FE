5b11cf412171e5898124199bfd42639c4adfa841
StavShar <stavshar21@gmail.com>
2023-06-13 17:17:57 +0300

    about.css added

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
+import './styles/About.css';
 
 function About () {
-    return (
-      <div className='main-container'>
-        <h1>About</h1>
-        <div>
-          <p className='about-par'>
-            This is our project in Project Managment course 2023
-          </p>
-        </div>
-        <div>
-          <h1>Authors:</h1>
-          <ul>
-            <strong>
-              <li> Stav Sharabi</li>
-              <li> Adar Katzir</li>
-              <li> Neoray Hagag</li>
-              <li> Anthony Epshtein</li>
-              <li> Ido Hazan</li>
-              <li> Maor Hadad</li>
-            </strong>
-          </ul>
-        </div>
+  return (
+    <div className='about'>
+      <h1>About</h1>
+      <div>
+        <p className='about-par'>
+          This is our project in Project Managment course 2023
+        </p>
       </div>
-    );
+      <div>
+        <h1>Authors:</h1>
+        <ul>
+          <strong>
+            <li> Stav Sharabi</li>
+            <li> Adar Katzir</li>
+            <li> Neoray Hagag</li>
+            <li> Anthony Epshtein</li>
+            <li> Ido Hazan</li>
+            <li> Maor Hadad</li>
+          </strong>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default About;
